24f1b9b2746b8d452c070789168421ddd5863ab3
HemanshuGupta <145928164+HemanshuGupta@users.noreply.github.com>
2025-06-18 00:15:32 +0530

    Delete oops11.java

--- oops11.java
+++
@@ -1,9 +0,0 @@
-//-Write a program to print your name, roll no, section and branch in separate lines.
-public class oops11 {
-    public static void main(String[] args){
-        System.out.println("Hemanshu Gupta");
-        System.out.println("2229034");
-        System.out.println("CSCE 2");
-        System.out.println("CSCE");
-    }
-}
